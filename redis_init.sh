10af759dca3034491a16d1156a59211d8162ebf7
biezhiwai <67008877+biezhiwai@users.noreply.github.com>
2022-10-25 20:13:12 +0800

    Update redis_init.sh

--- redis_init.sh
+++ redis_init.sh
@@ -1,6 +1,10 @@
+SUDOERS_FILE=/etc/sudoers
+SYSCTL_FILE=/etc/sysctl.conf
+REDIS_VERSION=7.0.5
+
 # 内核允许超量使用内存直到用完为止
-sudo echo "%sudo ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-sudo echo "vm.overcommit_memory = 1" | sudo tee -a /etc/sysctl.conf
+sudo echo "%sudo ALL=(ALL) NOPASSWD:ALL" | sudo tee -a $SUDOERS_FILE
+sudo echo "vm.overcommit_memory = 1" | sudo tee -a $SYSCTL_FILE
 sudo sysctl vm.overcommit_memory=1
 
 # 配置大内存页面
@@ -13,15 +17,15 @@ EOF
 # 关闭内存交换空间
 swapoff -a
 
-# 安装redis 7.0.5
+# 安装redis
 cd ~
 mkdir redis && cd redis
 mkdir bin conf data run log tls
 
-wget http://download.redis.io/releases/redis-7.0.5.tar.gz
-tar -xzvf redis-7.0.5.tar.gz
-cd redis-7.0.5
+wget http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
+tar -xzvf redis-${REDIS_VERSION}.tar.gz
+cd redis-${REDIS_VERSION}
 make -j 2
 make install PREFIX=~/redis
-cp redis-7.0.5/redis.conf redis/conf/
+cp redis-${REDIS_VERSION}/redis.conf redis/conf/
 # 然后修改redis.conf完成配置
